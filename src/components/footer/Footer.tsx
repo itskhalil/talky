c47bfa42081d0955cad2681eb95c91ee58d2be85
Khalil Mair <khalil.mair@helsing.ai>
2026-02-02 15:57:59 +0000

    chore:bump 0.1.3

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -13,7 +13,7 @@ const Footer: React.FC = () => {
         setVersion(appVersion);
       } catch (error) {
         console.error("Failed to get app version:", error);
-        setVersion("0.1.2");
+        setVersion("0.1.3");
       }
     };
 
